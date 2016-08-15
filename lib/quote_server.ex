1d4c0106b74b4adedde95904deb712ff6c3acf1b
Dmitry Begunkov <dbegunkov@gmail.com>
2016-08-16 01:04:30 +0300

    change mix.exs

--- lib/quote_server.ex
+++ lib/quote_server.ex
@@ -9,6 +9,7 @@ defmodule QuoteServer do
     ])
     _status = :application.start :ranch
     _status = :application.start :cowboy
+    _status = :application.start :quote_server
     {:ok, _} = :cowboy.start_http(:http, 100, [{:port, 8080}], [env: [dispatch: dispatch]])
   end
 end
